3e814b602140b944d1c1a4ca37ab3be0e81db51f
DolphinHyy <2428677341@qq.com>
2022-03-13 16:50:09 +0800

    完成组织架构基本结构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,6 +1,51 @@
 <template>
-  <div>
-    组织架构
+  <div class="dashboard-container">
+    <div class="app-container">
+      <!-- 头部栏 -->
+      <el-card class="box-card">
+        <el-row>
+          <el-col :span="20">传智教育</el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <el-col :span="10">负责人</el-col>
+              <el-col :span="10">
+                <el-dropdown>
+                  <span class="el-dropdown-link">
+                    操作<i class="el-icon-arrow-down el-icon--right" />
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+        <!-- 具体部门 -->
+        <el-tree default-expand-all :data="treeData" :props="defaultProps" @node-click="handleNodeClick">
+          <template>
+            <el-row style="width:100%">
+              <el-col :span="20">传智教育</el-col>
+              <el-col :span="4">
+                <el-row type="flex" justify="end">
+                  <el-col :span="10">负责人</el-col>
+                  <el-col :span="10">
+                    <el-dropdown>
+                      <span class="el-dropdown-link">
+                        操作<i class="el-icon-arrow-down el-icon--right" />
+                      </span>
+                      <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>添加子部门</el-dropdown-item>
+                      </el-dropdown-menu>
+                    </el-dropdown>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-tree>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -8,7 +53,13 @@
 export default {
   data() {
     return {
-
+      treeData: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
     }
   },
 
@@ -17,11 +68,12 @@ export default {
   },
 
   methods: {
-
+    handleNodeClick(data) {
+      console.log(data)
+    }
   }
 }
 </script>
 
 <style scoped lang='less'>
-
 </style>
